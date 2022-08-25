a591e51414f65f1270ccf1e41a78d9fa3f91906e
jtmiller <jtmiller@umn.edu>
2022-08-25 12:44:44 -0400

    cl

--- SpeciesPrimerEval_cl/SpeciesPrimerEval_cl_installation.sh
+++ SpeciesPrimerEval_cl/SpeciesPrimerEval_cl_installation.sh
@@ -2,4 +2,8 @@
 conda create --name qiime2R --file qiime2R.list.txt
 conda activate qiime2R
 
+chmod +x SpeciesPrimerEval_cl/SpeciesPrimerEval.R
+
 ## install R packages listed in SpeciesPrimerEval_cl_packages.R
+
+ex: Rscript SpeciesPrimerEval.R 'mifish' 12S-seqs-derep-teleo-seqs.qza 12S-tax-derep-uniq.qza 'herring' fish.txt
